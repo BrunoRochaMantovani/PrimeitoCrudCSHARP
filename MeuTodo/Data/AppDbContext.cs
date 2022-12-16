5cc384215625ab37950d02b9122763ca17b69c63
Bruno Rocha Mantovani <bruno.trabalho449243@outlook.com>
2022-12-16 14:36:44 -0300

    Projeto Crud API C SHARP

---
+++ MeuTodo/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MeuTodo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuTodo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        //Tabelas
+        public DbSet <Todo> Todos { get; set; }
+
+        //Conexão com o banco
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlite(connectionString: "DataSource=app.db; Cache=Shared");
+      
+    }
+}
---
+++ MeuTodo/ViewModels/CreateTodoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeuTodo.ViewModels
+{
+    public class CreateTodoViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+    }
+}
